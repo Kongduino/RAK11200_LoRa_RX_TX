f9c0a05db1e28bb02bf81eb5038590da2c19b383
Kongduino <dbvic@me.com>
2022-06-02 13:52:49 +0800

    Improved random number generation: a 256-byte stock is preloaded.

--- RAK11200_LoRa_RX_TX.ino
+++ RAK11200_LoRa_RX_TX.ino
@@ -1,3 +1,16 @@
+// Define LoRa parameters
+#define RF_FREQUENCY 868300000 // Hz
+#define TX_OUTPUT_POWER 22 // dBm
+#define LORA_BANDWIDTH 0 // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
+#define LORA_CODINGRATE 1 // [1: 4/5, 2: 4/6,  3: 4/7,  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8  // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0 // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+#define RX_TIMEOUT_VALUE 30000
+#define TX_TIMEOUT_VALUE 6000
+
 #include <Arduino.h>
 #include <SX126x-Arduino.h> //http://librarymanager/All#SX126x
 #include <SPI.h>
@@ -12,19 +25,6 @@ void OnRxError(void);
 void OnTxDone(void);
 void OnTxTimeout(void);
 void OnCadDone(bool);
-
-// Define LoRa parameters
-#define RF_FREQUENCY 868300000 // Hz
-#define TX_OUTPUT_POWER 22 // dBm
-#define LORA_BANDWIDTH 0 // [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
-#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
-#define LORA_CODINGRATE 1 // [1: 4/5, 2: 4/6,  3: 4/7,  4: 4/8]
-#define LORA_PREAMBLE_LENGTH 8  // Same for Tx and Rx
-#define LORA_SYMBOL_TIMEOUT 0 // Symbols
-#define LORA_FIX_LENGTH_PAYLOAD_ON false
-#define LORA_IQ_INVERSION_ON false
-#define RX_TIMEOUT_VALUE 30000
-#define TX_TIMEOUT_VALUE 6000
 
 static RadioEvents_t RadioEvents;
 static uint8_t TxdBuffer[64];
--- Sx1262LoRandom.h
+++ Sx1262LoRandom.h
@@ -40,13 +40,12 @@ void hexDump(uint8_t* buf, uint16_t len) {
 #define RANDOM_NUMBER_GENERATORBASEADDR 0x0819
 #define REG_ANA_LNA 0x08E2
 #define REG_ANA_MIXER 0x08E5
-uint8_t randomStock[256], randomPos=0;
+uint8_t randomStock[256], randomPos = 0;
 
-void fillRandom(uint8_t *myBuff, uint8_t stockSize) {
-  // myBuff
-  uint32_t number = 0;
+void fillUp() {
   uint8_t regAnaLna = 0, regAnaMixer = 0;
   uint16_t cnt = 0;
+  uint32_t number = 0;
   regAnaLna = Radio.Read(REG_ANA_LNA);
   Radio.Write(REG_ANA_LNA, regAnaLna & ~(1 << 0));
   regAnaMixer = Radio.Read(REG_ANA_MIXER);
@@ -54,17 +53,29 @@ void fillRandom(uint8_t *myBuff, uint8_t stockSize) {
   // Set radio in continuous reception
   Radio.Rx(0xFFFFFF);
   // SX126xSetRx(0xFFFFFF); // Rx Continuous
-  uint8_t tempSize = (stockSize / 4) + 1;
-  tempSize *= 4;
-  uint8_t tempBuff[tempSize];
-  while (cnt < tempSize) {
+  while (cnt < 256) {
     Radio.ReadBuffer(RANDOM_NUMBER_GENERATORBASEADDR, (uint8_t*)&number, 4);
     // Serial.println("number 0x" + String(number, HEX));
-    memcpy(tempBuff + cnt, (uint8_t*)&number, 4);
+    memcpy(randomStock + cnt, (uint8_t*)&number, 4);
     cnt += 4;
   }
-  memcpy(myBuff, tempBuff, stockSize);
+  randomPos = 0;
   Radio.Standby();
   Radio.Write(REG_ANA_LNA, regAnaLna);
   Radio.Write(REG_ANA_MIXER, regAnaMixer);
+  Radio.Rx(RX_TIMEOUT_VALUE);
+}
+
+void fillRandom(uint8_t *myBuff, uint8_t stockSize) {
+  if (randomPos == 0) fillUp();
+  uint8_t bytesLeft = 256 - randomPos;
+  if (stockSize <= bytesLeft) {
+    memcpy(myBuff, randomStock + randomPos, stockSize);
+    randomPos += stockSize;
+  } else {
+    memcpy(myBuff, randomStock + randomPos, bytesLeft);
+    fillUp();
+    memcpy(myBuff + bytesLeft, randomStock, stockSize - bytesLeft);
+    randomPos = stockSize - bytesLeft;
+  }
 }
